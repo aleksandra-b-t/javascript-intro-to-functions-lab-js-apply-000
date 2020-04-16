30cf0830841f6ede310f3613ebe4909f647d69ad
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 15:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  var caps = string.toUpperCase()
+  console.log(caps)
+}
+function logWhisper(string) {
+  var small = string.toLowerCase()
+  console.log(small)
+}
+
+function sayHiToGrandma(string) {
+  var myString = 'I love you, Grandma.'
+  for (var i = 0; i < string.length; i++) {
+    var letter = string[i]
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()){
+    return " YES INDEED!"
+  } else if (letter !== myString[i]) {
+    return "Cos nie tak" 
+  }
+  return 'I love you, too.'
+  } 
+}
